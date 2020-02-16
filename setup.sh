501422712ddbc1e7293afc1b48547d3eac2e450d
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2020-02-16 19:10:52 +1030

    bugfix bash ssh commands

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 # Arguments: 1, IP/URI to server
 # Assumptions: Working location includes secrets file
 
-jq -r .hetzner.ssh.privateKey ./secrets.json > ~/hetzner
+jq -r .hetzner.ssh.privateKey ./secrets.json > ~/.ssh/hetzner
 
-ssh "root@${0}" -f hetzner
+ssh "root@${0}" -i hetzner
 dnf install -y python36
 alias python=python3
 pip install hcloud
