21addb57d6a2a790ca28e61543511d51d55cbe39
Ariel Richtman <Ariel@Richtman.com.au>
2020-12-06 10:17:08 +0100

    at least basic auth works now

--- applications/build-protonmail-image.sh
+++ applications/build-protonmail-image.sh
@@ -8,5 +8,6 @@ cd ./docker-protonmail-bridge
 
 curl https://protonmail.com/download/beta/protonmail-bridge_${PROTONMAIL_BRIDGE_VERSION}_amd64.deb -O
 docker build . --build-arg SMTP_PORT=${PROTONMAIL_BRIDGE_SMTP_PORT} --build-arg IMAP_PORT=${PROTONMAIL_BRIDGE_IMAP_PORT} --build-arg PROTONMAIL_BRIDGE_VERSION=${PROTONMAIL_BRIDGE_VERSION} --tag ${PROTONMAIL_BRIDGE_IMAGE}
+docker tag ${PROTONMAIL_BRIDGE_IMAGE} "${PROTONMAIL_BRIDGE_IMAGE_NAME}:latest"
 
 popd
