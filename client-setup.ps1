f3119fd1211754c518ae6df21c3273a159e2f8bb
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2021-02-13 09:49:17 +0100

    added client-side setup

--- client-setup.ps1
+++ client-setup.ps1
@@ -1,1 +1,11 @@
+$Extensions = @(
+    ms-vscode-remote.remote-containers
+    ms-vscode-remote.remote-ssh
+    ms-vscode-remote.remote-ssh-edit
+    ms-vscode-remote.remote-wsl
+    ms-vscode-remote.vscode-remote-extensionpack
+)
+
+Install-Script Install-VSCode -Scope CurrentUser; Install-VSCode.ps1 -AdditionalExtensions $Extensions
+
 pip install hcloud
