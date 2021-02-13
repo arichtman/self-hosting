12d321d5eba745e7d158b8e7dbd40d9bfa608a9e
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2021-02-13 09:49:17 +0100

    tidy up

--- hardening/run-on-host.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-pushd /tmp/hardening
-
-dnf install -y ansible
-ansible-galaxy install git+https://github.com/openstack/ansible-hardening
-
-ansible-playbook ./playbook.yml
-
-popd
--- vscode-repair.sh
+++ vscode-repair.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
-# If this folder exists a VSCode Server install has been attempted already. Often triggered by trying to Remote-SSH before tar binary was available in PATH
-HASH=$(ls /root/.vscode-server/bin/ | head -n 1)
+# This script repairs a botched VSCode Server install.
+# Bad installs seem to triggered by trying to Remote-SSH before the tar binary was available in PATH. git binary may also be expected.
+# Best mitigation is simply installing the packages before attempting to connect but I went down the rabbit hole so I'm keeping it.
+
+# If this folder exists a VSCode Server install has been attempted already. HASH=$(ls /root/.vscode-server/bin/ | head -n 1)
 if [ $HASH ]
 then
 FOLDER="~/.vscode-server/bin/${HASH}"
@@ -9,5 +12,5 @@ curl --create-dirs -L "https://update.code.visualstudio.com/commit:${HASH}/serve
 cd $FOLDER
 tar -xvzf vscode-server-linux-x64.tar.gz --strip-components 1
 fi
-#TODO: Find cowsay/banner/figlet package for CentOS
+
 printf "\n\n***************\nYou may now use VSCode Remote-SSH extension\n***************\n"
