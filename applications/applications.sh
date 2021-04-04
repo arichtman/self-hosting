38f98a528d4988427339dd5fc0083452e907b4d5
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2021-04-04 11:15:53 +0200

    made progress

--- applications/applications.sh
+++ applications/applications.sh
@@ -1,12 +1,13 @@
 #! /bin/sh
 
-# dnf install -y httpd-tools certbot
+apt install -y docker.io docker-compose apache2-utils
+systemctl start docker
 
-apt install -y docker.io apache2-utils
-# systemctl start docker # Seems to start automagically when docker cli is used
+# Might need to review this, was really hoping for a github-aware package manager. I found *something* but this smells a little.
+unlink /bin/envsubst && echo "\n" | hubapp install a8m/envsubst && ln -s /usr/local/bin/envsubst /usr/bin/envsubst
 
 # Load use-case specific, secret variables into the session
-eval $(<./.env.private)
+set -a && eval "$(<./.env.private)" && set +a
 # Generate the compose environment file
 cat ./.env.tpl | envsubst > .env
 
@@ -15,18 +16,18 @@ mkdir -p ${BASE_DATA_LOCATION}/{letsencrypt,nextcloud,website,proxy,ttrss};
 mkdir -p {$NEXTCLOUD_HOST_WEB_DIR,$NEXTCLOUD_HOST_DB_DIR}
 
 # Prepare website files
-yes | cp ./www/* "${BASE_DATA_LOCATION}/website" ;
+cp ./www/* "${BASE_DATA_LOCATION}/website" ;
 cat ./www/index.html | envsubst > "${BASE_DATA_LOCATION}/website/index.html" ;
 
 # TODO: remove this, not sure traefik even supports hybrid configuration methods and we're definitely preferring the docker provider
 # TODO: rework the traefik static config for secure TLS only
-config_traefik()
-{
-    # There's definitely nicer approaches to shell templating than this
-    cat ./traefik.yaml.tpl | envsubst > "${BASE_DATA_LOCATION}/proxy/traefik.yaml"
-}
+# config_traefik()
+# {
+#     # There's definitely nicer approaches to shell templating than this
+#     cat ./traefik.yaml.tpl | envsubst > "${BASE_DATA_LOCATION}/proxy/traefik.yaml"
+# }
 
-config_traefik
+# config_traefik
 
 CERT_DETAILS_FILE="${BASE_DATA_LOCATION}/letsencrypt/acme.json";
 chmod 600 $CERT_DETAILS_FILE >> $CERT_DETAILS_FILE;
@@ -39,8 +40,5 @@ while ! docker-compose logs nextcloud | grep "Command line: 'apache2 -D FOREGROU
   sleep 10;
 done
 
-docker-compose exec -u www-data nextcloud php occ --no-interaction --quiet db:add-missing-indices
-docker-compose exec -u www-data nextcloud php occ --no-interaction --quiet db:convert-filecache-bigint
-
-# Clear the session variables?
-# exec bash -l
+docker-compose exec -u www-data cloud php occ --no-interaction --quiet db:add-missing-indices
+docker-compose exec -u www-data cloud php occ --no-interaction --quiet db:convert-filecache-bigint
