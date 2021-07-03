12a18faf661a99f758d8274df576c51207c8adc0
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2021-07-03 10:16:57 +0200

    disable password ssh authentication

--- server-setup.sh
+++ server-setup.sh
@@ -12,6 +12,10 @@ add-apt-repository ppa:rmescandon/yq
 apt update -y
 apt install -y yq
 
+# tune for vscode server performance
+echo "fs.inotify.max_user_watches=524288" > /etc/sysctl.d/11-vscode.conf
+service procps force-reload
+
 echo "https://$SECRET_GIT_USERNAME_ENCODED:$SECRET_GIT_PASSWORD_ENCODED@github.com" > ~/.git-credentials
 
 # Install a handy alias. I suspect as we move away from root this will come out.
@@ -26,6 +30,10 @@ printf "\n. /etc/bash_completion.d/yq" >> ~/.bashrc
 printf "\nset show-all-if-ambiguous on\n" >> ~/.inputrc
 
 exec bash -l
+
+# Disable password ssh auth
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+
 # TODO: adjust for Ubuntu
 # Permanently add interface for our floating IP
 #[ -z ${IP+x} ] && \
